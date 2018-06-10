fb60dc6a41d063d6cb7d20a33c5a91fbbe533bd0
Jithu Jose <jithumalayali@gmail.com>
2018-06-10 21:15:20 +0100

    Cleanup

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,61 +1,62 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class PostForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: '',
-            body: ''
-        }
-
-        this.onChange = this.onChange.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      body: ''
     }
 
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value })
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
     }
 
-    onSubmit(e) {
-        e.preventDefault()
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
 
-        const post = {
-            title: this.state.title,
-            body: this.state.body
-        }
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <pre style={{color: 'red'}}>23:14</pre>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title:</label><br/>
+            <input name="title" type="text" value={this.state.title} onChange={this.onChange}/>
+          </div>
+          <br/>
+          <div>
+            <label>Body</label><br/>
+            <textarea name="body" id="" cols="30" rows="10" value={this.state.body} onChange={this.onChange}/>
+          </div>
+          <br/>
+          <button type="submit">Submit</button>
 
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-        .then(resp => resp.json())
-        .then(data => console.log(data))
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Add Post</h1>
-                <form onSubmit={this.onSubmit}>
-                    <div>
-                        <label>Title:</label><br/>
-                        <input name="title" type="text" value={this.state.title} onChange={this.onChange}/>
-                    </div>
-                    <br/>
-                    <div>
-                        <label>Body</label><br/>
-                        <textarea name="body" id="" cols="30" rows="10" value={this.state.body} onChange={this.onChange}/>
-                    </div>
-                    <br/>
-                    <button type="submit">Submit</button>
-
-                </form>
-            </div>
-        )
-    }
+        </form>
+      </div>
+    )
+  }
 }
 
 export default PostForm
